ee4a3396dcb6da4ee966c018beba83964db3758b
david-zhou <david.zhou.tan@gmail.com>
2015-03-12 12:17:20 -0600

    Update spotifyLogin.php

--- app/views/spotifyLogin.php
+++ app/views/spotifyLogin.php
@@ -1,5 +1,5 @@
 <?php
-		$session = new SpotifyWebAPI\Session('78fe14946efe45f285d840b72bea40e4', '54db8e2ab9204bc08d09da67577c3bee', 'http://localhost:8000/');
+		$session = new SpotifyWebAPI\Session('API_KEY', 'SECRET', 'http://localhost:8000/');
 		$api = new SpotifyWebAPI\SpotifyWebAPI();
 		$session->getAuthorizeUrl(array('scope' => array('user-read-email', 'user-library-modify','user-read-private')),true);
 		//echo "<a href='" . $session->getAuthorizeUrl(array('scope' => array('user-read-email', 'user-library-modify','user-read-private')),true) . "'>Login</a>";
